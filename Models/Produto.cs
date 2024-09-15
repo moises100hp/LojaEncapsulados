192cec837d45c128a942e2ed0553a7a8f0824973
Moisés Gonçalves Silva <moises50hp@hotmail.com>
2024-09-14 22:30:48 -0300

    Add project files.

---
+++ Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace LojaEncapsuladosBlazor.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; } // Caminho da imagem do produto
+    }
+}

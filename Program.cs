ac1ee218f3124dcabf941cf1ec6903e4bea300a8
Moisés Gonçalves Silva <moises50hp@hotmail.com>
2024-09-15 17:37:17 -0300

    Ajustes

--- Program.cs
+++ Program.cs
@@ -6,4 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var app = builder.Build();
+
+
+// Configure other services or settings here if needed
+
+await app.RunAsync();
+
 await builder.Build().RunAsync();
